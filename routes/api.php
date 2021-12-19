ef0a98ea85f4ec4144b70b420cc476c03a3675bf
Daniel Okoronkwo <61119104+danielokoronkwo-coder@users.noreply.github.com>
2021-12-19 13:09:50 +0100

    CORS Abeg

--- routes/api.php
+++ routes/api.php
@@ -18,11 +18,11 @@ use App\Http\Controllers\PatientController;
 |
 */
 
-header("Cache-Control: no-cache, must-revalidate");
-header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
-header('Access-Control-Allow-Origin:  *');
-header('Access-Control-Allow-Methods:  POST, GET, OPTIONS, PUT, DELETE');
-header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Origin, Authorization');
+// header("Cache-Control: no-cache, must-revalidate");
+// header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+// header('Access-Control-Allow-Origin:  *');
+// header('Access-Control-Allow-Methods:  POST, GET, OPTIONS, PUT, DELETE');
+// header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Origin, Authorization');
 
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
