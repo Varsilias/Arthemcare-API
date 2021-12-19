bbc313a79ddcba75255f286abec593b50c6462f0
Daniel Okoronkwo <61119104+danielokoronkwo-coder@users.noreply.github.com>
2021-12-19 13:41:18 +0100

    CORS Abeg

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*', 'POST', 'GET', 'OPTIONS', 'PUT', 'DELETE'],
 
-    'allowed_origins' => ['https://arthemcare.vercel.app', 'http://localhost:3000', '*'],
+    'allowed_origins' => ['https://arthemcare.vercel.app/', 'http://localhost:3000', '*'],
 
     'allowed_origins_patterns' => [],
 
