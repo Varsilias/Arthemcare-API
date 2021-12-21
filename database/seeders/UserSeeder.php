12e772733d8c9df7b6300a1b445e0388a0e68451
Daniel Okoronkwo <61119104+danielokoronkwo-coder@users.noreply.github.com>
2021-12-21 15:36:36 +0100

    Added two extra default users with respective role

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -20,8 +20,8 @@ class UserSeeder extends Seeder
     public function run()
     {
         $doctor = Role::create(['name' => 'Doctor']);
-        Role::create(['name' => 'Nurse']);
-        Role::create(['name' => 'FrontDesk Staff']);
+        $nurse = Role::create(['name' => 'Nurse']);
+        $staff = Role::create(['name' => 'FrontDesk Staff']);
 
         $daniel = \App\Models\User::create([
             'firstname' => "Daniel",
@@ -36,6 +36,32 @@ class UserSeeder extends Seeder
             'remember_token' => Str::random(10),
         ]);
         $daniel->assignRole($doctor);
+
+        $sopulu = \App\Models\User::create([
+            'firstname' => "Prosper",
+            'lastname' => "Nnacheta",
+            'gender' => "male",
+            'DOB' => "29-10-1997",
+            'username' => "Nacheetah",
+            'email' => "nacheetah70@gmail.com",
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+        ]);
+        $sopulu->assignRole($staff);
+
+        $delia = \App\Models\User::create([
+            'firstname' => "Cordelia",
+            'lastname' => "Ukpai",
+            'gender' => "female",
+            'DOB' => "28-10-2001",
+            'username' => "Delia",
+            'email' => "deliaukpai@gmail.com",
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+        ]);
+        $delia->assignRole($nurse);
 
         foreach(Role::all() as $role) {
             $users = \App\Models\User::factory(10)->create();
